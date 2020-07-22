b977d8ffdf0dfd83bc3943118a845881d0b40bb9
justin-curtis-lu <67398801+justin-curtis-lu@users.noreply.github.com>
2020-07-21 23:15:23 -0700

    Create ApiTest.py

---
+++ ApiTest.py
@@ -0,0 +1,7 @@
+
+import requests
+
+response = requests.get("http://api.open-notify.org/iss-now.json")
+# Print the status code of the response.
+print("The ISS is currently located at :")
+print(response.content)

d1b80ea556dd145ace5f480ce21522c9ac47ec86
justin-curtis-lu <67398801+justin-curtis-lu@users.noreply.github.com>
2020-07-22 17:10:13 -0700

    Delete ApiTest.py

--- ApiTest.py
+++
@@ -1,7 +0,0 @@
-# test on random API
-import requests
-
-response = requests.get("http://api.open-notify.org/iss-now.json")
-# Print the status code of the response.
-print("The ISS is currently located at :")
-print(response.content)

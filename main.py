6009aff53d7885756ee1e6aab983b52ea9a6cbba
Nate Yeo <nathan@theyeos.com>
2020-07-26 16:57:50 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+import os
+from PlayerRoster import PlayerRoster
+
+def main():
+    roster = PlayerRoster()
+    x = roster.createPlayerRoster()
+    print(x)
+    
+
+if __name__ == "__main__":
+	main()

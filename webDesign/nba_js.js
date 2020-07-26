1131c2a2bfb40f57e7ae09bda36c01e4e4b2a719
justin-curtis-lu <67398801+justin-curtis-lu@users.noreply.github.com>
2020-07-25 18:56:36 -0700

    Add files via upload

--- webDesign/nba_js.js
+++ webDesign/nba_js.js
@@ -1,4 +1,5 @@
 /* Nba external Js link */
+
 var preLoad;
 function loading() {
   preLoad = setTimeout(showPage, 3000);
@@ -7,3 +8,20 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
+
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});

0ebd8680ceb34ddbe3ade8ba4a235f40e52bd0da
Nate Yeo <nathan@theyeos.com>
2020-07-27 22:21:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from PlayerRoster import PlayerRoster
 def main():
     roster = PlayerRoster()
     x = roster.createPlayerRoster()
+	
+    for value in x.values():
+        for i in value:
+            print("<option value=" + '"' + i[0] + " " + i[1] + "|" + i[3] + '"' + ">" + i[0] + " " + i[1] + "</option>")
+
     print(x)
     
 

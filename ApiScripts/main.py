934b6d4f0d0ff6a7213df6b48a944f7b4170321f
Nate Yeo <nathan@theyeos.com>
2020-07-28 19:41:51 -0700

    Update main.py

--- ApiScripts/main.py
+++ ApiScripts/main.py
@@ -5,13 +5,10 @@ from PlayerRoster import PlayerRoster
 def main():
     roster = PlayerRoster()
     x = roster.createPlayerRoster()
-	
     for value in x.values():
         for i in value:
             print("<option value=" + '"' + i[0] + " " + i[1] + "|" + i[3] + '"' + ">" + i[0] + " " + i[1] + "</option>")
-
     print(x)
-    
 
 if __name__ == "__main__":
 	main()
